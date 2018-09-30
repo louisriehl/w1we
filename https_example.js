b74ef4bccd8ce0c9fdf21c1d507b5f8170835738
Louis Riehl <louisriehl@gmail.com>
2018-09-30 15:06:27 +0000

    successfully made request to example.org

--- https_example.js
+++ https_example.js
@@ -1,3 +1,19 @@
 var https = require('https'); //gets the https module
 
 console.log("https module aquired!");
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+//called by https when request is made
+var callback = function() {
+  console.log("Responding to handler callback!");
+};
+
+console.log("I'm about to make a request...");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
